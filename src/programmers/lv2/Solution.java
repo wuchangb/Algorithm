eb36a3d1db745fb438ddae0b6d8209cbd2e8f3a0
wuchang <kkang0820qus@gmail.com>
2023-08-22 10:26:40 +0900

    programmers 문제 추가

---
+++ src/programmers/lv2/Solution.java
@@ -0,0 +1,28 @@
+package programmers.lv2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+
+    static ArrayList<String> list = new ArrayList<>();
+    static String[] arr = {"A", "E", "I", "O", "U"};
+
+    public int solution(String word) {
+        brute(0,"");
+        Collections.sort(list);
+        int idx = list.indexOf(word);
+        return idx+1;
+    }
+
+    public void brute(int idx, String word) {
+        if (idx == 5) {
+            return;
+        }
+        for (int i = 0; i < 5; i++) {
+            String s = word + arr[i];
+            list.add(s);
+            brute(idx + 1, s);
+        }
+    }
+}
